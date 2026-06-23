85a4738fbed51c92119d63a16bf0801d5a44bc2d
SilkCitrus <silkcitruss@gmail.com>
2026-06-23 03:08:14 +0100

    NPC ticket dawg
    added some basic code for the ticket dawg interation, it moves too

--- Assets/Scripts/NPC/NPCInteract.cs
+++ Assets/Scripts/NPC/NPCInteract.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,7 +9,17 @@ public class NPCInteract : MonoBehaviour
 
     public string sceneName;
 
+    public bool isDefeated;
+    public GameObject NPCBooth;
+    public GameObject NPC;
+    public List<Sprite> ObjectStates;
+
     public string NpcName { get => npcName; }
+
+    private void Start()
+    {
+        NPCBooth.GetComponent<SpriteRenderer>().sprite = ObjectStates[0];
+    }
 
     public void Interact()
     {
@@ -16,4 +27,15 @@ public class NPCInteract : MonoBehaviour
 
         SceneManager.LoadScene(sceneName);
     }
+
+    private void Update()
+    {
+        if (isDefeated == true)
+        {
+            NPCBooth.GetComponent<SpriteRenderer>().sprite = ObjectStates[1];
+            NPC.GetComponent<Animator>().SetBool("hasDefeated", true);
+
+        }
+    }
+
 }
