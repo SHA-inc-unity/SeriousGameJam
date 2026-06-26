47b0ba8ad3303612b0d2df3484b9db04f34ccbe2
SHA-inc-unity <85645846+SHA-inc-unity@users.noreply.github.com>
2026-06-27 02:00:26 +0300

    STOP TOUCHING THE DOGS

--- Assets/_Scripts/Battle/Overworld/GuardDog.cs
+++ Assets/_Scripts/Battle/Overworld/GuardDog.cs
@@ -1,14 +1,41 @@
+using System.Collections;
 using UnityEngine;
 
 public class DogGuard : MonoBehaviour
 {
-    [SerializeField] private BattleTrigger[] battles;
+    [SerializeField] private Door door;
+    [SerializeField] private Transform goOff;
+    [SerializeField] private float moveDuration = 1f;
+
+    private bool hasMoved = false;
 
     private void Update()
     {
-        foreach (var battle in battles)
-            if (!battle.NPCInteract.isDefeated) return;
+        if (hasMoved) return;
 
-        gameObject.SetActive(false);
+        if (door.IsOpen)
+        {
+            hasMoved = true;
+            StartCoroutine(MoveAway());
+        }
+    }
+
+    private IEnumerator MoveAway()
+    {
+        Vector3 from = transform.position;
+        Vector3 to = goOff.position;
+        float t = 0f;
+
+        while (t < moveDuration)
+        {
+            t += Time.deltaTime;
+            transform.position = Vector3.Lerp(from, to, t / moveDuration);
+            yield return null;
+        }
+
+        transform.position = to;
+
+        BoxCollider box = GetComponent<BoxCollider>();
+        if (box != null) box.enabled = false;
     }
 }
